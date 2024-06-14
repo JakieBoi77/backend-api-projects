27cc608335fb092672e063144c19487d169fc354
JakieBoi77 <jakef7@icloud.com>
2024-06-14 12:48:59 -0400

    completed file metadata microservice

--- file-metadata-microservice/index.js
+++ file-metadata-microservice/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
 require('dotenv').config()
 
 var app = express();
@@ -14,4 +15,23 @@ app.get('/', function (req, res) {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+// Storage Engine
+const storage = multer.memoryStorage();
+
+// Initialize Upload
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 }
+});
+
+// File Analyzer Endpoint
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+  res.json(fileInfo);
 });
